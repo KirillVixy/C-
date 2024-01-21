ebc9044818b24f98ff0369532517833e526d8c04
Levi Ackerman <layrdmastermemof@gmail.com>
2024-01-21 20:36:38 +0300

    Complete Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+    void Main(int num)
+    { 
+        if (num % 7 == 0 && num % 23 == 0)
+        {
+            System.Console.WriteLine("да");
+        }
+        else
+        {
+            System.Console.WriteLine("нет");
+        }
+    }
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Main(num);

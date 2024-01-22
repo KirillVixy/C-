b72d8ccc282b1459ac01f510d1df8534de43029c
Levi Ackerman <layrdmastermemof@gmail.com>
2024-01-22 23:58:12 +0300

    Complete task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+static void Main()
+{
+    Console.Write("Введите натуральное число n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n < 10)
+    {
+        Console.WriteLine(n);
+    }
+    else
+    {
+        while (n > 0)
+        {
+            int Num1 = n % 10;
+            n /= 10;
+            if (n > 0)
+            {
+               Console.Write(Num1 + ",");
+            }
+            else
+            {
+                Console.WriteLine(Num1);
+            }
+        }
+    }
+}
+Main();
+
+
+
+
+

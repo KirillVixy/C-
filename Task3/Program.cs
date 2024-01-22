aa287a9eacdf389db6a1a1819148036f4af9c97d
Levi Ackerman <layrdmastermemof@gmail.com>
2024-01-22 23:26:33 +0300

    Complete Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+static void Main()
+{
+    Console.Write("Введите число из отрезка [10, 99]: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if(number >= 10 && number <= 99)
+    {
+        int firstDigit = number / 10;
+        int SecondDigit = number % 10;
+        int maxDigit = firstDigit > SecondDigit ? firstDigit : SecondDigit;
+        Console.WriteLine(maxDigit);
+    }
+    else 
+    {
+        System.Console.WriteLine("NO");
+    }
+
+
+}
+Main();
+
+
+
+

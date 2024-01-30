e94579f482040602ded00f4651ca0e761a99cbfc
Levi Ackerman <layrdmastermemof@gmail.com>
2024-01-30 20:29:54 +0300

    Задание 2

---
+++ sem4/Task1/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿static void Main()
+{
+    while (true)
+    {
+        Console.Write("Введите число или 'q' для выхода: ");
+        
+        string input = Console.ReadLine();
+        if (input == "q") 
+        {
+            break;
+        } 
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (int.TryParse(input, out number))
+        {
+
+      
+            int sum = 0;
+            while (number > 0) 
+            {
+                sum += number % 10;
+                number /= 10; 
+            }
+            if (sum % 2 == 0) 
+            {
+                Console.WriteLine("Break");
+                break;
+            }
+        }
+        else 
+        {
+            Console.WriteLine("Ничего не вышло");
+        }
+   }
+}
+Main();
+
+
+
---
+++ sem4/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] CreateRandomArray(int size)
+{
+    int max = 999;
+    int min = 100;
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{  
+    for (int i = 0; i < array.Length; i++)
+    {  
+        System.Console.Write(array[i] + " ");
+        
+    }
+    Console.Write("Четных чисел: ");
+    Console.WriteLine(CountEvenItems(array));
+
+}
+
+int CountEvenItems(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+                count++;
+        }
+    }
+    return count;
+}
+PrintArray(CreateRandomArray(10));

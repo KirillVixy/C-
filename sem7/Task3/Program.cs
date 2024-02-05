11697da649d9a68ebfd9ecb745c64ec2188aa2e0
Levi Ackerman <layrdmastermemof@gmail.com>
2024-02-06 01:02:55 +0300

    семинар 7 Задание 3

---
+++ sem7/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int Main(int n)
+{
+    int[] array = {1, 2, 5, 0, 10, 34};
+
+    if(n < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        Console.Write(array[n] + " ");
+        return Main(n - 1);
+    }
+}
+Main(5);
